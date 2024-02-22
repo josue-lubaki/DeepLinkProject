6908c87b2d735c039229d1d0e8293a8b2a033bc8
Josue Lubaki <josuelubaki@gmail.com>
2024-02-22 15:15:56 -0500

    added deep link

--- app/src/main/java/ca/josue_lubaki/deeplinkproject/MainActivity.kt
+++ app/src/main/java/ca/josue_lubaki/deeplinkproject/MainActivity.kt
@@ -1,15 +1,22 @@
 package ca.josue_lubaki.deeplinkproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
 import ca.josue_lubaki.deeplinkproject.ui.theme.DeepLinkProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +24,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DeepLinkProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                val uri = "https://josue-lubaki.ca"
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
                 ) {
-                    Greeting("Android")
+                    composable(
+                        route= "home",
+                        deepLinks = listOf(
+                            navDeepLink {
+                                uriPattern = "$uri/{id}"
+                                action = Intent.ACTION_VIEW
+                            }
+                        )
+                    ){ backStackEntry ->
+                        val arguments = backStackEntry.arguments
+                        val id = arguments?.getString("id")
+
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = id ?: "No id passed")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DeepLinkProjectTheme {
-        Greeting("Android")
     }
 }
